61555d66b9170b0b160ab36b5821701ac13af8bf
adel-hart <bepuetv123@gmail.com>
2024-03-18 09:30:05 +0900

    set class

--- Codefile/main.py
+++ Codefile/main.py
@@ -1,2 +1,11 @@
-import simpy
+class node():
 
+    def __init__(self):
+        self.Sex = 0;
+        self.rootM = 0 #index num
+        self.rootF = 0 #index num
+
+        self.geneA = [] #ex AA, aa
+        self.geneB = [] #ex BB, Bb
+
+        
